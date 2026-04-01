239c86ee29110cf1c7a7b0e992088afaf6677f68
Pedro <rm94786@estudante.fieb.edu.br>
2026-04-01 13:59:45 +0000

    Carrossel

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -27,18 +27,94 @@ function Home() {
           <p>Plataforma de cursos online</p>
         </div>
 
-        <div className="carousel">
-          <div className="carousel-item">
-            <img src="/ImgCurso1.jpg"></img>
-          </div>
-          <div className="carousel-item">
-            <img src="/ImgCurso2.jpg"></img>
-          </div>
-          <div className="carousel-item">
-            <img src="/ImgCurso3.jpg"></img>
+        <div className="card">
+          <div className="carousel">
+            <div className="carousel-item">
+              <img src="/1.jpg"></img>
+            </div>
+            <div className="carousel-item">
+              <img src="/2.jpg"></img>
+            </div>
+            <div className="carousel-item">
+              <img src="/3.jpg"></img>
+            </div>
           </div>
         </div>
 
+        --------
+            <div class="options">
+
+        <div class="option active"
+             style="--optionImage: url('https://images.unsplash.com/photo-1501785888041-af3ef285b470?w=1200');">
+            <div class="label">
+                <div class="icon"><i class="fas fa-walking"></i></div>
+                <div class="info">
+                    <div class="main">Nature Walk</div>
+                    <div class="sub">Beautiful mountain scenery</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="option"
+             style="--optionImage: url('https://images.unsplash.com/photo-1470770841072-f978cf4d019e?w=1200');">
+            <div class="label">
+                <div class="icon"><i class="fas fa-tree"></i></div>
+                <div class="info">
+                    <div class="main">Forest</div>
+                    <div class="sub">Green & peaceful environment</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="option"
+             style="--optionImage: url('https://images.unsplash.com/photo-1482192596544-9eb780fc7f66?w=1200');">
+            <div class="label">
+                <div class="icon"><i class="fas fa-mountain"></i></div>
+                <div class="info">
+                    <div class="main">Mountains</div>
+                    <div class="sub">Snowy high peaks</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="option"
+             style="--optionImage: url('https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?w=1200');">
+            <div class="label">
+                <div class="icon"><i class="fas fa-water"></i></div>
+                <div class="info">
+                    <div class="main">Lake View</div>
+                    <div class="sub">Calm and relaxing water</div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <script>
+        const options = document.querySelectorAll(".option");
+        let currentIndex = 0;
+
+        // Manual click activation
+        options.forEach((option, index) => {
+            option.addEventListener("click", () => {
+                setActive(index);
+            });
+        });
+
+        // Function to activate selected slide
+        function setActive(index) {
+            options.forEach(o => o.classList.remove("active"));
+            options[index].classList.add("active");
+            currentIndex = index;
+        }
+
+        // Auto slide every 3 seconds
+        setInterval(() => {
+            currentIndex = (currentIndex + 1) % options.length;
+            setActive(currentIndex);
+        }, 3000);
+    </script>
+    ----------
         <div className="card">
           <h2>Sobre a Plataforma</h2>
           <p>
